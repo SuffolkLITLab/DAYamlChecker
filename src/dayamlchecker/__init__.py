a5c7540c89305c46a42df1daa1093283c81f2ce4
Quinten Steenhuis <qsteenhuis@gmail.com>
2026-07-07 11:53:02 -0400

    Install code block def warning

--- src/dayamlchecker/__init__.py
+++ src/dayamlchecker/__init__.py
@@ -5,6 +5,10 @@ from dayamlchecker.yaml_structure import (
     find_errors_from_string,
     find_style_findings_from_string,
 )
+from dayamlchecker._code_def_warning import install as _install_code_def_warning
+
+_install_code_def_warning()
+del _install_code_def_warning
 
 __all__ = [
     "Finding",
